a6e657d3b68168b0f6f98afc60fe78f66d427e80
Ganesh <shewaleg140@gmail.com>
2026-06-07 01:45:31 +0530

    Navbar created....

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/landing/home/home.component';
+import { BooksComponent } from './features/landing/books/books.component';
+import { AboutComponent } from './features/landing/about/about.component';
+import { ContactComponent } from './features/landing/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'books', component: BooksComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'contact', component: ContactComponent },
+];
